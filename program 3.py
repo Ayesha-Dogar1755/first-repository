988a23a63402312688765ebf1c3a14f84f4b08cd
Ayesha-Dogar1755 <2004amnadogar@gmail.com>
2026-01-16 00:21:01 +0500

    Add files via upload

---
+++ program 3.py
@@ -0,0 +1,14 @@
+#Taking input from user ang give output
+name=input("Enter your name:")
+age=input("Enter your age:")
+height=input("Enter your height:")
+edu=input("Enter your Qualification:")
+institute_name=input("Enter your college/University name:")
+degree=input("Enter your degree:")
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Education:",edu)
+print("Institute Name:",institute_name)
+print("Degree:",degree)
+
