11e532b3bd02318237528220aa114016ffee6686
Ayesha-Dogar1755 <2004amnadogar@gmail.com>
2026-01-16 00:09:14 +0500

    Add files via upload

---
+++ program 2.py
@@ -0,0 +1,17 @@
+
+# Taking input and performing arithematic operations and giving output20
+value_1=int(input("Enter a value:"))  #int is used Because input operation take every value as string.
+value_2=int(input("Enter another value:"))
+add=value_1+value_2
+sub=value_1-value_2
+multi=value_1*value_2
+div=value_1/value_2
+float_div=value_1//value_2
+modulus=value_1%value_2
+print("Add:",add)
+print("Subtraction:",sub)
+print("Multiply:",multi)
+print("Divsion:",div)
+print("Float_Division:",float_div)
+print("Modulus:",modulus)
+
