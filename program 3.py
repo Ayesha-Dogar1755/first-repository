501c8911db14a08e4b156f3ebeba14d219f9496e
Ayesha-Dogar1755 <2004amnadogar@gmail.com>
2026-01-16 23:38:19 +0500

    Initial commit

--- program 3.py
+++ program 3.py
@@ -1,4 +1,4 @@
-#Taking input from user and give output
+#Taking input from user , check data type and give output
 name=input("Enter your name:")
 age=int(input("Enter your age:"))  #input always take string.That's why we should use to convert numbers in int.
 height=int(input("Enter your height:"))  #input always take string.That's why we should use to convert numbers in int.
