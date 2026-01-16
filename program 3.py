c8793cfbb50d986e77a9bfcbccb58c2c5cfb3fc8
Ayesha-Dogar1755 <2004amnadogar@gmail.com>
2026-01-16 23:11:36 +0500

    Fiest Contribution

--- program 3.py
+++ program 3.py
@@ -1,14 +1,14 @@
-#Taking input from user ang give output
+#Taking input from user and give output
 name=input("Enter your name:")
-age=input("Enter your age:")
-height=input("Enter your height:")
+age=int(input("Enter your age:"))  #input always take string.That's why we should use to convert numbers in int.
+height=int(input("Enter your height:"))  #input always take string.That's why we should use to convert numbers in int.
 edu=input("Enter your Qualification:")
 institute_name=input("Enter your college/University name:")
 degree=input("Enter your degree:")
-print("Name:",name)
-print("Age:",age)
-print("Height:",height)
-print("Education:",edu)
-print("Institute Name:",institute_name)
-print("Degree:",degree)
+print("Name:",name,type(name))
+print("Age:",age,type(age))
+print("Height:",height,type(height))
+print("Education:",edu,type(edu))
+print("Institute Name:",institute_name,type(institute_name))
+print("Degree:",degree,type(degree))
 
