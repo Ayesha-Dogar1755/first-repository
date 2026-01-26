a20d39fb7f6635b2f1218e7e586dcf642bf0cf7b
Ayesha-Dogar1755 <2004amnadogar@gmail.com>
2026-01-26 23:55:21 +0500

    Day 9 task

--- Day 8 task/program 43.py
+++ Day 8 task/program 43.py
@@ -3,8 +3,10 @@ cities=["Faisalabad","Lahore","Islamabad","Karachi","Peshawar","Multan","Quetta"
 Food=["Biryani","Nihari","Kebabs","Pulao","Karahi","Sajji","Rice pudding","Halwa","Chapati"]
 Games=["Cricket","Hockey","Football","Badminton","Squash","Tennis"]
 Languages=["Punjabi","Urdu","English","Sindhi","Pashto"]
+# function definition:
 def len_of_list(list):
     print(len(list))
+#function calls:   
 len_of_list(cities) 
 len_of_list(Food)
 len_of_list(Games)
